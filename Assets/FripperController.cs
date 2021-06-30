140b7833847ccc0e7a7d82d5d5119cc7d4cc2424
nogamijisan <86593262+nogamijisan@users.noreply.github.com>
2021-06-30 17:19:37 +0900

    Initial commit

---
+++ Assets/FripperController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+
+
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+    //Touch touch0;
+    //Touch touch1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //touch0 = Input.GetTouch(0);
+        //touch1 = Input.GetTouch(1);
+
+        bool leftTouchDown = false; 
+        bool rightTouchDown = false; 
+
+        bool leftTouchUp = false; 
+        bool rightTouchUp = false; 
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (touch.position.x < 540)
+                {
+                    leftTouchDown = true; 
+                }
+                else if(touch.position.x >= 540)
+                {
+                    rightTouchDown = true; 
+                }
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (touch.position.x < 540)
+                {
+                    leftTouchUp = true; 
+                }
+                else if (touch.position.x >= 540)
+                {
+                    rightTouchUp = true; 
+                }
+            }
+        }
+
+
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)
+            || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)
+            || leftTouchDown)
+            && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)
+            || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)
+            || rightTouchDown)
+            && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)
+            || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)
+            || leftTouchUp)
+            && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)
+            || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)
+            || rightTouchUp)
+            && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+    }
+
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+
+    private int score;
+    private GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = this.score.ToString();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //private string otag = other.gameObject.tag;
+
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += 5;
+        }
+        else if(other.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += 15;
+        }
+            
+    }
+}
